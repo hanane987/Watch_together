6831fe16ed6a2405b81aaebf17afc715da266828
hanane987 <aminahanane987@gmail.com>
2024-11-24 20:17:34 +0100

    Feat : Adding the playlist page in the pages folder

--- frontend/src/pages/PlaylistPage.tsx
+++ frontend/src/pages/PlaylistPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PlaylistForm from '../components/PlaylistForm';
-import { fetchPlaylists } from '../redux/slices/playlistSlice';
+
 
 const PlaylistPage = () => {
   const dispatch = useDispatch();
@@ -24,4 +24,4 @@ const PlaylistPage = () => {
   );
 };
 
-export default PlaylistPage;
+   export default PlaylistPage;
