910a6da8e4a094a2e72575e607bf37d9a303a472
hanane987 <aminahanane987@gmail.com>
2024-11-24 20:19:19 +0100

    Feat : Adding the roompage in the pages

--- frontend/src/pages/PlaylistPage.tsx
+++ frontend/src/pages/PlaylistPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PlaylistForm from '../components/PlaylistForm';
-
+import { fetchPlaylists } from '../redux/slices/playlistSlice';
 
 const PlaylistPage = () => {
   const dispatch = useDispatch();
